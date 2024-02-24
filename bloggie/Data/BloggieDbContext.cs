656cd926e4819444a7e07141ecdd6991c36041bc
Onuralp Kaygın <onuralpkayginedu@gmail.com>
2024-02-24 15:38:25 +0300

    migrations added

---
+++ bloggie/Data/BloggieDbContext.cs
@@ -0,0 +1,16 @@
+﻿using bloggie.Models.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace bloggie.Data
+{
+    public class BloggieDbContext : DbContext
+    {
+        public BloggieDbContext(DbContextOptions options) : base(options)
+        {        
+        }
+
+        public DbSet<BlogPost> BlogPosts { get; set; }
+        public DbSet<Tag> Tags { get; set; }
+
+    }
+}
